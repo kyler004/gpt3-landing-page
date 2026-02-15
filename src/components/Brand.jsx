da6f4977c3bc48baa3e0591b621b53847f7a6fde
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-02-15 12:53:32 +0100

    Completed the brand componenent

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const Brand = () => {
-  return <div>Brand</div>;
-};
+import google from "../assets/google.png";
+import slack from "../assets/slack.png";
+import atlassian from "../assets/atlassian.png";
+import dropbox from "../assets/dropbox.png";
+import shopify from "../assets/shopify.png";
+
+const Brand = () => (
+  <div className="flex flex-wrap justify-center items-center section__padding">
+    <div className="flex-1 max-w-37.5 min-w-30 m-4 flex justify-center items-center">
+      <img src={google} />
+    </div>
+    <div className="flex-1 max-w-37.5 min-w-30 m-4 flex justify-center items-center">
+      <img src={slack} />
+    </div>
+    <div className="flex-1 max-w-37.5 min-w-30 m-4 flex justify-center items-center">
+      <img src={atlassian} />
+    </div>
+    <div className="flex-1 max-w-37.5 min-w-30 m-4 flex justify-center items-center">
+      <img src={dropbox} />
+    </div>
+    <div className="flex-1 max-w-37.5 min-w-30 m-4 flex justify-center items-center">
+      <img src={shopify} />
+    </div>
+  </div>
+);
 
 export default Brand;
