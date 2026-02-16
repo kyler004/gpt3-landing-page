f71968a9afce63cff4f7236267241ae0060c8ea8
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-02-16 16:50:10 +0100

    Completed the base structure of the project. Ready for upgrades

--- src/containers/Possibility.jsx
+++ src/containers/Possibility.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import possibilityImage from "../assets/possibility.png";
 
-const Possibility = () => {
-  return (
-    <div>Possibility</div>
-  )
-}
+const Possibility = () => (
+  <div className="flex flex-col lg:flex-row" id="possibility">
+    <div className="flex-1 flex justify-start items-center lg:mr-8">
+      <img src={possibilityImage} alt="possibility" className="w-full h-full" />
+    </div>
+    <div className="flex-1 flex flex-col justify-end items-start mt-8 lg:mt-0">
+      <h4 className="font-medium text-lg leading-7 text-cyan-400">
+        Request Early Access to Get Started
+      </h4>
+      <h1 className="text-2xl md:text-3xl font-extrabold leading-snug gradient__text my-4">
+        The possibilities are <br /> beyond your imagination
+      </h1>
+      <p className="text-base leading-7 text-text mb-8">
+        Yet bed any for travelling assistance indulgence unpleasing. Not
+        thoughts all exercise blessing. Indulgence way everything joy alteration
+        boisterous the attachment. Party we years to order allow asked of.
+      </p>
+      <h4 className="font-medium text-lg leading-7 text-orange-400">
+        Request Early Access to Get Started
+      </h4>
+    </div>
+  </div>
+);
 
-export default Possibility
+export default Possibility;
--- src/containers/WhatGPT3.jsx
+++ src/containers/WhatGPT3.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import Feature from "../components/Feature";
 
-const WhatGPT3 = () => {
-  return (
-    <div>WhatGPT3</div>
-  )
-}
+const WhatGPT3 = () => (
+  <div
+    className="gpt3__whatgpt3"
+    id="wgpt3"
+  >
+    <div className="flex">
+      <Feature
+        title="What is GPT-3"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+      />
+    </div>
+    <div className="flex flex-col md:flex-row justify-between items-center my-16">
+      <h1 className="text-2xl md:text-3xl font-extrabold leading-snug max-w-127.5 gradient__text">
+        The possibilities are beyond your imagination
+      </h1>
+      <p className="text-base leading-7 text-orange-400 cursor-pointer mt-4 md:mt-0">
+        Explore the Library
+      </p>
+    </div>
+    <div className="flex flex-wrap mt-8">
+      <Feature
+        title="Chatbots"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+      />
+      <Feature
+        title="Knowledgebase"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+      <Feature
+        title="Education"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+    </div>
+  </div>
+);
 
-export default WhatGPT3
+export default WhatGPT3;
